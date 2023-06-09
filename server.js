5466f83d3d23fe82b67d7260f68020c7fe16618c
Reza <arianpourinfo@gmail.com>
2023-06-09 18:50:37 +0330

    fix cors

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const tronRoutes = require('./routes/tron');
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(express.json());
 app.use('/api/tron', tronRoutes);
